8610c46b08f58f24668d355a28da969b2c52d1a9
mateuszborycki@gmail.com <mateuszborycki@gmail.com@c5491469-7843-cbc3-d8e7-a1e26d208431>
2012-01-29 02:08:18 +0000

    - added simple title screen

--- trunk/MyXLib/MyXLib/TitleScreen.cpp
+++ trunk/MyXLib/MyXLib/TitleScreen.cpp
@@ -3,15 +3,18 @@
 #include "GameResources.h"
 #include "../MXLib/MXAnimUtils.h"
 #include "../MXLib/MXScript.h"
+#include "../MXLib/MXSpriter.h"
+
+
 
 namespace MX
 {
 
 
 
-
 void InitializeTitle(const shared_ptr<MX::Draw> &_draw, const shared_ptr<MX::Spriter> &_spriter, MX::Scene *_scene)
 {
+
 	static auto title_background = MX::CreateAnimationFromFile(*_draw, L"images\\splash1.png");
 	title_background->frames[0].centerX = 0.0f;
 	title_background->frames[0].centerY = 0.0f;
@@ -23,6 +26,7 @@ void InitializeTitle(const shared_ptr<MX::Draw> &_draw, const shared_ptr<MX::Spr
 	splash1->OnDo.connect(q(wait(1000), lerp_color(0xFFFFFFFF, 400000), die()));
 
 	_scene->AddActor(splash1);
+	
 }
 
 };
