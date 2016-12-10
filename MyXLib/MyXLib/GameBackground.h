96df01477be353a161dbfa1a69939fb2fda9054a
Mateusz Borycki <mateuszborycki@gmail.com>
2016-12-10 22:05:32 +0100

    Remove all.

--- MyXLib/MyXLib/GameBackground.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <memory>
-#include "../MXLib/MXAnimation.h"
-#include "../MXLib/MXScene.h"
-#include "../MXLib/MXSpriter.h"
-
-extern void InitBackground(MX::Scene *_scene, const std::shared_ptr<MX::Spriter> &_spriter);
